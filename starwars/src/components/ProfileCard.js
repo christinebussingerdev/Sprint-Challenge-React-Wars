8750939eed229f18c65222cd916a20f7a7587a95
unknown <christinebussingerdev@gmail.com>
2019-09-29 21:31:33 -0400

    finished mvp

---
+++ starwars/src/components/ProfileCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Card, CardText, CardTitle} from 'reactstrap';
+
+export default function ProfileCard(props) {
+
+    return (
+        <Card className="character-profile">
+            <CardTitle><h2>{props.name}</h2></CardTitle>
+            <CardText>Height: {props.height}</CardText>
+            <CardText>Gender: {props.gender}</CardText>
+            <CardText>Eye Color: {props.eye_color}</CardText>
+            <CardText>Hair Color: {props.hair_color}</CardText>
+        </Card>
+    )
+}
